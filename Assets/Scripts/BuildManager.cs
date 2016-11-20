44260cc56683264e0e13923769ad2a74c6096317
Colin Monroe <cmmonroe2010@gmail.com>
2016-11-19 19:54:11 -0500

    initial commit.
    Finally started using git with unity don't know why it took me so damn
    look to do this.

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildManager : MonoBehaviour {
+    public GameObject BasePrefab;
+    public GameObject BarracksPrefab;
+    public Camera MainCamera;
+
+    private GameObject activeObject;
+    private const int buildingLayerMask = 1 << 9;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (activeObject)
+        {
+            RaycastHit hit;
+            Ray ray = MainCamera.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out hit, 50f, buildingLayerMask);
+            activeObject.transform.position = hit.point;
+            //if (Input.GetMouseButtonDown(0) && !firstClick)
+            //{
+            //    activeObject.transform.position = MainCamera.ScreenToWorldPoint(Input.mousePosition);
+            //    activeObject = null;
+            //}
+        }
+	}
+
+    public void CreateBase()
+    {
+        activeObject = GameObject.Instantiate<GameObject>(BasePrefab);
+        activeObject.transform.position = MainCamera.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
