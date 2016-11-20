78a4c786c9c07e698758b6b2431ec9c80c5171b3
Vigenere <cmmonroe2010@gmail.com>
2016-11-20 13:09:54 -0500

    small updates

--- Assets/Scripts/BuildingBehavior.cs
+++ Assets/Scripts/BuildingBehavior.cs
@@ -5,33 +5,49 @@ public class BuildingBehavior : MonoBehaviour {
     public Color HighlightColor;
     private Color starterColor;
 
+
+    private bool selected;
+
 	// Use this for initialization
 	void Start () {
         starterColor = gameObject.GetComponent<Renderer>().material.color;
 	}
 	// Update is called once per frame
 	void Update () {
+        if (selected)
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                SpawnHero();
+            }
+        }
 	
 	}
     public void Select()
     {
         gameObject.GetComponent<Renderer>().material.color = HighlightColor;
         SetChildrenToColor(HighlightColor);
+        selected = true;
     }
     public void Deselect()
     {
         gameObject.GetComponent<Renderer>().material.color = starterColor;
         SetChildrenToColor(starterColor);
+        selected = false;
     }
     void SetChildrenToColor(Color setColor)
     {
         Renderer[] rends = gameObject.GetComponentsInChildren<Renderer>();
         if (rends != null)
         {
-            foreach (var child in rends)
+            foreach (var renderer in rends)
             {
-                child.material.color = setColor;
+                renderer.material.color = setColor;
             }
         }
+    }
+    void SpawnHero()
+    {
+
     }
 }
